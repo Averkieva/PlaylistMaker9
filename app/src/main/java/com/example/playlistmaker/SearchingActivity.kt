2749b5e975b648b6caf440da763cdcee392e63c1
Ksenia <averkieva10012017@yandex.ru>
2023-04-26 18:19:42 +0300

    2nd hw

--- app/src/main/java/com/example/playlistmaker/SearchingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchingActivity.kt
@@ -1,11 +1,64 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchingActivity : AppCompatActivity() {
+    private lateinit var inputEditText: EditText
+
+    companion object {
+        const val QUERY = "QUERY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searching)
+        val backButton = findViewById<ImageView>(R.id.returnButton)
+        inputEditText = findViewById(R.id.inputEditText)
+        backButton.setOnClickListener {
+            this.finish()
+        }
+        val clearButton = findViewById<ImageView>(R.id.cancel_button)
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            keyboard.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+            inputEditText.clearFocus()
+
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    clearButton.visibility = View.GONE
+                } else {
+                    clearButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(QUERY, inputEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val searchQuery = savedInstanceState.getString(QUERY, "")
+        inputEditText.setText(searchQuery)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -11,6 +14,28 @@ class SettingsActivity : AppCompatActivity() {
         val backButton = findViewById<ImageView>(R.id.returnButton)
         backButton.setOnClickListener {
             this.finish()
+        }
+        val shareButton = findViewById<TextView>(R.id.share)
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_developer_url))
+            startActivity(intent)
+        }
+        val supportButton = findViewById<TextView>(R.id.support)
+        supportButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.email))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.sub))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.txt))
+            startActivity(intent)
+        }
+        val userPolicyButton = findViewById<TextView>(R.id.userPolicy)
+        userPolicyButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = (Uri.parse(getString(R.string.practicum_offer)))
+            startActivity(intent)
         }
     }
 }
