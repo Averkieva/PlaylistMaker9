1fad266a676ef582c11a07cc9619bd1da38d95c8
Ksenia <averkieva10012017@yandex.ru>
2023-08-18 19:55:46 +0300

    11th hw media player_2

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -4,7 +4,6 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -12,35 +11,36 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import java.text.SimpleDateFormat
+import java.util.*
 
 class AudioPlayerActivity : AppCompatActivity() {
 
-    enum class StatesOfPlaying{
+    enum class StatesOfPlaying {
         STATE_DEFAULT,
         STATE_PREPARED,
         STATE_PLAYING,
         STATE_PAUSED
     }
 
-    private lateinit var audioPlayerTrackName:TextView
-    private lateinit var audioPlayerArtistName:TextView
-    private lateinit var audioPlayerTime:TextView
-    private lateinit var audioPlayerAlbumName:TextView
-    private lateinit var audioPlayerYearNumber:TextView
-    private lateinit var audioPlayerGenreName:TextView
-    private lateinit var audioPlayerCountryName:TextView
-    private lateinit var audioPlayerCover:ImageView
-    private lateinit var audioPlayerBackButton:ImageButton
-    private lateinit var audioPlayerPlayButton:ImageButton
-    private lateinit var audioPlayerPauseButton:ImageButton
-    private lateinit var audioPlayerTrackTimer:TextView
+    private lateinit var audioPlayerTrackName: TextView
+    private lateinit var audioPlayerArtistName: TextView
+    private lateinit var audioPlayerTime: TextView
+    private lateinit var audioPlayerAlbumName: TextView
+    private lateinit var audioPlayerYearNumber: TextView
+    private lateinit var audioPlayerGenreName: TextView
+    private lateinit var audioPlayerCountryName: TextView
+    private lateinit var audioPlayerCover: ImageView
+    private lateinit var audioPlayerBackButton: ImageButton
+    private lateinit var audioPlayerPlayButton: ImageButton
+    private lateinit var audioPlayerTrackTimer: TextView
 
     private val mediaPlayer = MediaPlayer()
     private var mainThreadHandler: Handler? = null
     private var statesOfPlaying = StatesOfPlaying.STATE_DEFAULT
+    private var timeRunnable: Runnable = Runnable {}
     var time = ""
 
-    companion object{
+    companion object {
         const val KEY_TRACK_NAME = "Track Name"
         const val KEY_TRACK_ARTIST = "Artist Name"
         const val KEY_TRACK_TIME = "Track Time"
@@ -60,18 +60,24 @@ class AudioPlayerActivity : AppCompatActivity() {
         setContentView(R.layout.audio_player)
         initViews()
         audioPlayerPlayButton.setOnClickListener { playBackControl() }
-        audioPlayerPauseButton.setOnClickListener { playBackControl() }
         audioPlayerBackButton.setOnClickListener {
             finish()
         }
 
-        audioPlayerTrackName.text = intent.extras?.getString(KEY_TRACK_NAME) ?: getString(R.string.unknownTrack)
-        audioPlayerArtistName.text = intent.extras?.getString(KEY_TRACK_ARTIST) ?: getString(R.string.unknownArtist)
-        audioPlayerTime.text = intent.extras?.getString(KEY_TRACK_TIME) ?: getString(R.string.track_time)
-        audioPlayerAlbumName.text = intent.extras?.getString(KEY_TRACK_ALBUM) ?: getString(R.string.unknownAlbum)
-        audioPlayerYearNumber.text = (intent.extras?.getString(KEY_TRACK_YEAR) ?: getString(R.string.unknownYear)).take(year)
-        audioPlayerGenreName.text = intent.extras?.getString(KEY_TRACK_GENRE) ?: getString(R.string.unknownGenre)
-        audioPlayerCountryName.text = intent.extras?.getString(KEY_TRACK_COUNTRY) ?: getString(R.string.unknownCountry)
+        audioPlayerTrackName.text =
+            intent.extras?.getString(KEY_TRACK_NAME) ?: getString(R.string.unknownTrack)
+        audioPlayerArtistName.text =
+            intent.extras?.getString(KEY_TRACK_ARTIST) ?: getString(R.string.unknownArtist)
+        audioPlayerTime.text =
+            intent.extras?.getString(KEY_TRACK_TIME) ?: getString(R.string.track_time)
+        audioPlayerAlbumName.text =
+            intent.extras?.getString(KEY_TRACK_ALBUM) ?: getString(R.string.unknownAlbum)
+        audioPlayerYearNumber.text =
+            (intent.extras?.getString(KEY_TRACK_YEAR) ?: getString(R.string.unknownYear)).take(year)
+        audioPlayerGenreName.text =
+            intent.extras?.getString(KEY_TRACK_GENRE) ?: getString(R.string.unknownGenre)
+        audioPlayerCountryName.text =
+            intent.extras?.getString(KEY_TRACK_COUNTRY) ?: getString(R.string.unknownCountry)
 
         val image = (intent.extras?.getString(KEY_TRACK_COVER))
 
@@ -89,53 +95,54 @@ class AudioPlayerActivity : AppCompatActivity() {
         if (!url.isNullOrEmpty())
             preparePlayer(url)
     }
-    private fun preparePlayer(url: String){
+
+    private fun preparePlayer(url: String) {
         mediaPlayer.setDataSource(url)
         mediaPlayer.prepareAsync()
-        mediaPlayer.setOnPreparedListener{
+        mediaPlayer.setOnPreparedListener {
             audioPlayerPlayButton.isEnabled = true
             statesOfPlaying = StatesOfPlaying.STATE_PREPARED
-            audioPlayerPlayButton.visibility = View.VISIBLE
-            audioPlayerPauseButton.visibility = View.GONE
         }
-        mediaPlayer.setOnCompletionListener{
+        mediaPlayer.setOnCompletionListener {
+            audioPlayerPlayButton.setImageResource(R.drawable.play_button)
             statesOfPlaying = StatesOfPlaying.STATE_PREPARED
-            audioPlayerPlayButton.visibility = View.VISIBLE
-            audioPlayerPauseButton.visibility = View.GONE
+            audioPlayerTrackTimer.text = resources.getString(R.string.track_time)
+            mainThreadHandler?.removeCallbacks(timeRunnable)
         }
     }
 
-    private fun startPlayer(){
+    private fun startPlayer() {
         mediaPlayer.start()
         statesOfPlaying = StatesOfPlaying.STATE_PLAYING
-        audioPlayerPlayButton.visibility = View.GONE
-        audioPlayerPauseButton.visibility = View.VISIBLE
-        mainThreadHandler?.post(duration())
+        audioPlayerPlayButton.setImageResource(R.drawable.audio_player_pause)
+        duration()
     }
 
-    private fun pausePlayer(){
+    private fun pausePlayer() {
         super.onPause()
         mediaPlayer.pause()
         statesOfPlaying = StatesOfPlaying.STATE_PAUSED
-        audioPlayerPlayButton.visibility = View.VISIBLE
-        audioPlayerPauseButton.visibility = View.GONE
+        audioPlayerPlayButton.setImageResource(R.drawable.play_button)
     }
 
-    private fun playBackControl(){
-        when (statesOfPlaying){
-            StatesOfPlaying.STATE_PLAYING->{
+    private fun playBackControl() {
+        when (statesOfPlaying) {
+            StatesOfPlaying.STATE_PLAYING -> {
                 pausePlayer()
             }
-            StatesOfPlaying.STATE_PREPARED, StatesOfPlaying.STATE_PAUSED->{
+            StatesOfPlaying.STATE_PREPARED, StatesOfPlaying.STATE_PAUSED -> {
                 startPlayer()
             }
-            else->{}
+            else -> {}
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer.release()
+        if(statesOfPlaying == StatesOfPlaying.STATE_DEFAULT) {
+            mediaPlayer.release()
+            mainThreadHandler?.removeCallbacks(timeRunnable)
+        }
     }
 
     override fun onPause() {
@@ -143,27 +150,17 @@ class AudioPlayerActivity : AppCompatActivity() {
         if (mediaPlayer.isPlaying)
             mediaPlayer.pause()
         statesOfPlaying = StatesOfPlaying.STATE_PAUSED
-        audioPlayerPlayButton.visibility = View.VISIBLE
-        audioPlayerPauseButton.visibility = View.GONE
+        audioPlayerPlayButton.setImageResource(R.drawable.play_button)
     }
 
-    private fun duration():Runnable{
-        return object:Runnable{
-            override fun run(){
-                if (statesOfPlaying==StatesOfPlaying.STATE_PLAYING){
-                    time = SimpleDateFormat("mm:ss").format(mediaPlayer.currentPosition)
-                    audioPlayerTrackTimer.text = time
-                    mainThreadHandler?.postDelayed(this, AUDIO_DELAY_MILLIS)
-                }
-                else{
-                    audioPlayerTrackTimer.text = "00:00"
-                    mainThreadHandler?.post(this)
-                }
-            }
-        }
+    private fun duration() {
+        time = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+        audioPlayerTrackTimer.text = time
+        timeRunnable = Runnable { duration() }
+        mainThreadHandler?.postDelayed(timeRunnable, AUDIO_DELAY_MILLIS)
     }
 
-    private fun initViews(){
+    private fun initViews() {
         audioPlayerTrackName = findViewById(R.id.audioPlayerTrackName)
         audioPlayerArtistName = findViewById(R.id.audioPlayerArtistName)
         audioPlayerTime = findViewById(R.id.audioPlayerTime)
@@ -174,7 +171,6 @@ class AudioPlayerActivity : AppCompatActivity() {
         audioPlayerCover = findViewById(R.id.audioPlayerCover)
         audioPlayerBackButton = findViewById(R.id.audioPlayerBackButton)
         audioPlayerPlayButton = findViewById(R.id.audioPlayerPlayButton)
-        audioPlayerPauseButton = findViewById(R.id.audioPlayerPauseButton)
         audioPlayerTrackTimer = findViewById(R.id.audioPlayerTrackTimer)
         mainThreadHandler = Handler(Looper.getMainLooper())
     }
